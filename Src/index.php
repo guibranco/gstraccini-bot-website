4af35a1be9fc72cb5f16a66353fbef18c4b6580e
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-02-14 22:08:58 +0000

    Update index.php

--- Src/index.php
+++ Src/index.php
@@ -544,7 +544,6 @@
                                                                 </tr>
                                                                 <tr>
                                                                     <td align="center" class="w95">
-                                                                      <p dir="auto">That's what I can do <img title=":neckbeard:" alt=":neckbeard:" src="https://github.githubassets.com/images/icons/emoji/neckbeard.png" height="20" width="20" /></p>
                                                                         <ul>
                                                                             <li><a href="https://github.com/marketplace/gstraccini-bot">GitHub Marketplace</a></li>
                                                                             <li><a href="https://github.com/apps/gstraccini">GitHub App</a></li>
