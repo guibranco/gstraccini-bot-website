82f92a056a8794caf2976894352ac12f190e7764
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-14 00:52:25 +0100

    Update index4.php

--- Src/index4.php
+++ Src/index4.php
@@ -144,7 +144,9 @@
 
 <body>
   <header>
-    <h1>GStraccini-bot</h1>
+    <a href="https://bot.straccini.com">
+      <img src="https://raw.githubusercontent.com/guibranco/gstraccini-bot-website/main/Src/logo.png" alt="GStraccini-bot Logo" class="logo" alt="GStraccini-bot logo" >
+    </a>
     <p>Automate your GitHub workflow effortlessly</p>
   </header>
 
