6a42cde2b915ea2a00ccb9395a5f177878b10405
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-14 00:52:48 +0100

    Update index.php

--- Src/index.php
+++ Src/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GStraccini Bot</title>
+    <title>GStraccini-bot - Automate Your Workflow</title>
     <style type="text/css">
         u+#body a {
             color: #4c7af1;
