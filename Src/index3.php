f7274d13cb7253652eef0e4896bfc284c878e3e3
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-14 00:55:04 +0100

    Update index3.php

--- Src/index3.php
+++ Src/index3.php
@@ -123,7 +123,7 @@
     <a href="https://bot.straccini.com">
       <img src="https://raw.githubusercontent.com/guibranco/gstraccini-bot-website/main/Src/logo.png" alt="GStraccini-bot Logo" class="logo" alt="GStraccini-bot logo" >
     </a>
-    <p>The GitHub bot to keep your repository organized and healthy.</p>
+    <p>🤖 <img src="https://github.githubassets.com/images/icons/emoji/octocat.png" alt="GitHub Octocat" class="octocat"> Automate your GitHub workflow effortlessly.</p>
   </header>
 
   <section>
