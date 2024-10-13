ef4628368810d18b67c38270683b695fec489e90
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-14 00:55:02 +0100

    Update index2.php

--- Src/index2.php
+++ Src/index2.php
@@ -110,9 +110,7 @@
     <a href="https://bot.straccini.com">
       <img src="https://raw.githubusercontent.com/guibranco/gstraccini-bot-website/main/Src/logo.png" alt="GStraccini-bot Logo" class="logo" alt="GStraccini-bot logo" >
     </a>
-    <p>🤖 <img src="https://github.githubassets.com/images/icons/emoji/octocat.png" alt="GitHub Octocat"
-        class="octocat"> A GitHub bot that automates
-      tasks, manages pull requests, issues, comments, and commits.</p>
+    <p>🤖 <img src="https://github.githubassets.com/images/icons/emoji/octocat.png" alt="GitHub Octocat" class="octocat"> Automate your GitHub workflow effortlessly.</p>
     <img src="https://bot.straccini.com/gstraccini-bot.png" alt="GStraccini-bot">
     <div class="badges">
       <a href="https://github.com/guibranco/gstraccini-bot/actions/workflows/deploy.yml">
