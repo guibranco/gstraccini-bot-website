a9ceb7645e5620bfc261cc984535d7c5ce4fda5f
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-15 18:59:35 +0100

    Update dashboard.php (#123)
    Co-authored-by: gstraccini[bot] <150967461+gstraccini[bot]@users.noreply.github.com>

--- Src/dashboard.php
+++ Src/dashboard.php
@@ -25,6 +25,7 @@ $title = "Activity Dashboard";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GStraccini-bot | <?php echo $title; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="user.css">
 </head>
 
@@ -175,7 +176,7 @@ $title = "Activity Dashboard";
         </div>
     </div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
+    <?php require_once "includes/footer.php"; ?>    
     <script>
         function populateRepositoriesTable(repositories) {
             const repositoriesTable = document.getElementById('repositories');
