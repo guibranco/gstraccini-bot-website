15af9dca3da7ed1110dc1c65237bcddc21fde4a9
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-16 20:17:59 +0100

    Update api.php

--- Src/api.php
+++ Src/api.php
@@ -1,4 +1,13 @@
 <?php
+$cookie_lifetime = 604800;
+session_set_cookie_params([
+    'lifetime' => $cookie_lifetime,
+    'path' => '/',
+    'domain' => 'bot.straccini.com',
+    'secure' => true,
+    'httponly' => true,
+    'samesite' => 'Strict'
+]);
 session_start();
 
 $data = ['error' => 'Unauthorized'];
