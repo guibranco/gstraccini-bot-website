6eb7e82b6b7a90639b47562fd6e338d89fa56ede
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-16 20:22:47 +0100

    Update login.php

--- Src/login.php
+++ Src/login.php
@@ -1,5 +1,19 @@
 <?php
+$cookie_lifetime = 604800;
+session_set_cookie_params([
+    'lifetime' => $cookie_lifetime,
+    'path' => '/',
+    'domain' => 'bot.straccini.com',
+    'secure' => true,
+    'httponly' => true,
+    'samesite' => 'Strict'
+]);
+session_start();
+
 require_once "github.secrets.php";
-$authorizeUrl = "https://github.com/login/oauth/authorize?client_id=$gitHubClientId&redirect_uri=" . urlencode($gitHubRedirectUri);
+$redirect_uri = urlencode($gitHubRedirectUri); 
+$state = bin2hex(random_bytes(16));
+$_SESSION['oauth_state'] = $state;
+$authorizeUrl = "https://github.com/login/oauth/authorize?client_id=$gitHubClientId&redirect_uri=$url&state=$state";
 header("Location: $authorizeUrl");
 exit();
