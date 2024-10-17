fc456c5454ee672870c26966bbb05a69c1b69dc8
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-17 09:10:18 +0100

    Update login.php (#185)

--- Src/login.php
+++ Src/login.php
@@ -14,6 +14,6 @@ require_once "github.secrets.php";
 $redirect_uri = urlencode($gitHubRedirectUri); 
 $state = bin2hex(random_bytes(16));
 $_SESSION['oauth_state'] = $state;
-$authorizeUrl = "https://github.com/login/oauth/authorize?client_id=$gitHubClientId&redirect_uri=$url&state=$state";
+$authorizeUrl = "https://github.com/login/oauth/authorize?client_id={$gitHubClientId}&redirect_uri={$redirect_uri}&state={$state}";
 header("Location: $authorizeUrl");
 exit();
