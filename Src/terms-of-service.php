2d4259eff1d5f9634320a7c3e8294bd53165c0ed
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-13 16:26:22 +0000

    Update terms-of-service.php

--- Src/terms-of-service.php
+++ Src/terms-of-service.php
@@ -168,19 +168,7 @@ $isAuthenticated = isset($_SESSION['user']);
     <p>Thank you for using GStraccini-bot!</p>
   </div>
 
-  <footer>
-    <p>© 2024 GStraccini-bot. All rights reserved.</p>
-    <nav>
-      <ul>
-        <li><a href="privacy-policy.php">Privacy Policy</a></li>
-        <li><a href="terms-of-service.php">Terms of Service</a></li>
-        <li><a href="service-status.php">Service Status</a></li>
-        <li><a href="https://docs.bot.straccini.com" target="_blank">Documentation</a></li>
-        <li><a href="https://github.com/marketplace/gstraccini-bot" target="_blank">GitHub Marketplace</a></li>
-        <li><a href="https://github.com/guibranco/gstraccini-bot-service" target="_blank">GitHub Repository</a></li>
-      </ul>
-    </nav>
-  </footer>
+  <?php include_once "includes/footer_public.php"; ?>
 </body>
 
 </html>
