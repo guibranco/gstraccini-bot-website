35a202926d4ea6fd80352cdcbdfecc05ba477b63
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-14 20:08:15 +0000

    Update footer.php (#300)

--- Src/includes/footer.php
+++ Src/includes/footer.php
@@ -1,3 +1,17 @@
+<footer>
+  <p>© 2024 GStraccini-bot. All rights reserved.</p>
+  <nav>
+    <ul>
+      <li><a href="privacy-policy.php">Privacy Policy</a></li>
+      <li><a href="terms-of-service.php">Terms of Service</a></li>
+      <li><a href="service-status.php">Service Status</a></li>
+      <li><a href="https://docs.bot.straccini.com" target="_blank">Documentation</a></li>
+      <li><a href="https://github.com/marketplace/gstraccini-bot" target="_blank">GitHub Marketplace</a></li>
+      <li><a href="https://github.com/guibranco/gstraccini-bot-service" target="_blank">GitHub Repository</a></li>
+    </ul>
+  </nav>
+</footer>
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
