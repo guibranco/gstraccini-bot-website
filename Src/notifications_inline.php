142a4bec650f6184d4476d1f13dfbcb21e6e36ce
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-12-21 01:38:48 +0000

    fix - change header method for better HTTP response handling

--- Src/notifications_inline.php
+++ Src/notifications_inline.php
@@ -2,7 +2,7 @@
 require_once "includes/session.php";
 
 if ($isAuthenticated === false) {
-    header('HTTP/1.1 401 Unauthorized');
+    http_response_code(401); 
     echo json_encode(['error' => 'User not authenticated.']);
     exit();
 }
