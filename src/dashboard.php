b153a1c00eb60201be814a8e0e514131e0d41960
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-01-21 19:16:38 +0000

    Update dashboard.php

--- src/dashboard.php
+++ src/dashboard.php
@@ -161,8 +161,9 @@ if (isset($user["first_name"])) {
                             <span class="text-muted">
                                 <a href='https://github.com/<?php echo htmlspecialchars($issue['full_name']); ?>'
                                     target='_blank'><?php echo htmlspecialchars($issue['repository']); ?></a>
-                            </span> -
-                            <span class="text-muted">(🕐 <?php echo $issue['created_at']; ?>)</span>
+                            </span>
+                            <br />
+                            <span class="text-muted">🕐 <?php echo $issue['created_at']; ?></span>
                             <?php if (isset($issue["state"]) && $issue["state"] === "success") { ?>
                                 <span class="badge bg-success">
                                     <i class="fas fa-check-circle"></i> Success
@@ -257,8 +258,9 @@ if (isset($user["first_name"])) {
                 itemLi.className = 'list-group-item';
                 let content = '';
                 content += `<strong><a href='${item.url}' target='_blank'>${item.title}</a></strong><br />`;
-                content += `<span class="text-muted"><a href='https://github.com/${item.full_name}' target='_blank'>${item.repository}</a></span> - `;
-                content += `<span class="text-muted">(🕐 ${item.created_at})</span> ${state}`;
+                content += `<span class="text-muted"><a href='https://github.com/${item.full_name}' target='_blank'>${item.repository}</a></span><br />`;
+                content += `<span class="text-muted">🕐 ${item.created_at}</span>`
+                content += state;
                 itemLi.innerHTML = content;
                 list.appendChild(itemLi);
             });
