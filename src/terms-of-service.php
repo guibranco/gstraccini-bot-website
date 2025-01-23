a58019ea7c29bc1f489ec5288de1e75e9b1bffd5
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-01-23 10:38:58 +0000

    Update terms-of-service.php

--- src/terms-of-service.php
+++ src/terms-of-service.php
@@ -8,7 +8,7 @@ require_once "includes/session.php";
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="GStraccini-bot Terms of Service">
-  <title>GStraccini-bot Terms of Service</title>
+  <title>GStraccini Bot | Terms of Service</title>
   <link rel="stylesheet" href="/static/main.css" />
 </head>
 
