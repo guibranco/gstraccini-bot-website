138db44a69fde35fdcd081409288a9c667e40576
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-01-23 10:40:37 +0000

    Update notifications.php

--- src/notifications.php
+++ src/notifications.php
@@ -16,7 +16,7 @@ $title = "Notifications";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GStraccini-bot | <?php echo $title; ?></title>
+    <title>GStraccini Bot | <?php echo $title; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="/static/user.css">
