e75c951cc2974d769516f3047251179c20cf9b32
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-01-23 10:39:20 +0000

    Update signin.php

--- src/signin.php
+++ src/signin.php
@@ -24,7 +24,7 @@ if ($isAuthenticated === true) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>GStraccini Bot | Login Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="/static/main.css" />
